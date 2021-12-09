71940886b59b0f7d30bbc82334a9e39c9f162664
Borja Gómez Gimeno <borja.gomez@volkswagen-groupservices.com>
2021-12-09 18:38:13 +0100

    Fix compilation error

--- gazpacho/src/state/reducers/sessionReducer.spec.js
+++ gazpacho/src/state/reducers/sessionReducer.spec.js
@@ -42,7 +42,6 @@ describe("sessionReducer", () => {
     });
 
     expect(Object.keys(actual)).toHaveLength(1);
-    expect(actual.hasOwnProperty(dateKey)).toBeTruthy();
     expect(actual[dateKey]).toEqual(
       expect.arrayContaining([newSession, oldSessionObject])
     );
--- gazpacho/src/state/storeDefinition.ts
+++ gazpacho/src/state/storeDefinition.ts
@@ -1,9 +1,12 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-export const store = createStore({
-  reducer: {
-    posts: postsReducer,
-    comments: commentsReducer,
-    users: usersReducer,
-  },
-});
+// export const store = createStore({
+//   reducer: {
+//     posts: postsReducer,
+//     comments: commentsReducer,
+//     users: usersReducer,
+//   },
+// });
+export function mock() {
+  return null;
+}
